e4817efe836a8e0ebc51ad9c8e1caf68307aebb4
Raju Ghimire <raju.805421@cct.tu.edu.np>
2025-05-13 21:20:31 +0545

    Initial

---
+++ setup.ts
@@ -0,0 +1,37 @@
+// Install TypeScript
+
+// npm install -g typescript
+// Installs the TypeScript compiler globally.
+// You can now use the tsc command in your terminal.
+// tsc stands for TypeScript Compiler.
+
+
+
+// Check Installation
+
+// tsc --version
+// Confirms installation and shows the installed version.
+// Make sure it prints something like 5.x.x.
+
+
+//  Initialize a Project
+
+// mkdir ts-learning
+// cd ts-learning
+// npm init -y
+// tsc --init
+
+
+//  Create First TS File
+
+// touch index.ts
+// Inside index.ts, write:
+// let message: string = "Hello TypeScript!";
+// console.log(message);
+
+
+
+// Compile & Run
+
+// tsc index.ts      # Compiles index.ts to index.js
+// node index.js     # Runs the compiled JS file
